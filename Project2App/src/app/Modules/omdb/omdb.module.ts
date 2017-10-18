a97f79c60b63e28637c76f02279f60be2a8dc254
James Martini <martinij@udel.edu>
2017-10-18 16:42:04 -0400

    Updated files.

--- Project2App/src/app/Modules/omdb/omdb.module.ts
+++ Project2App/src/app/Modules/omdb/omdb.module.ts
@@ -1,25 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Http, HttpModule } from '@angular/http';
-//import { Routes, RouterModule } from '@angular/router';
-
-/* HIS EXAMPLE FOR ROUTES
-const routes: Routes = [
-  {path: '', redirectTo: 'artist'},
-  {path: 'artist', component: ArtistsComponent},
-  {path: 'track', component: TracksComponent}
-];*/
-
-export function OMDBfactory(http: Http){
-  return new OMDBService(http, 'http://www.omdbapi.com/');
-}
 
 @NgModule({
   imports: [
-    CommonModule,
-    HttpModule
+    CommonModule
   ],
-  declarations: [],
-  providers: [{provide: OMDBService, useFactory: OMDBfactory, deps: [Http]}],
+  declarations: []
 })
 export class OmdbModule { }
