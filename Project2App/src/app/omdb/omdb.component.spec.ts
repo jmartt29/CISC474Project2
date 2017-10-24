224d99d25db29a96514801137e3d4f2669ca57c1
David Geron-Neubauer <davidgn@udel.edu>
2017-10-24 16:18:53 -0400

    Spaces -> Tabs

--- Project2App/src/app/omdb/omdb.component.spec.ts
+++ Project2App/src/app/omdb/omdb.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { OmdbComponent } from './omdb.component';
 
 describe('OmdbComponent', () => {
-  let component: OmdbComponent;
-  let fixture: ComponentFixture<OmdbComponent>;
+	let component: OmdbComponent;
+	let fixture: ComponentFixture<OmdbComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ OmdbComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [OmdbComponent]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OmdbComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(OmdbComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- Project2App/src/app/omdb/omdb.service.spec.ts
+++ Project2App/src/app/omdb/omdb.service.spec.ts
@@ -3,13 +3,13 @@ import { TestBed, inject } from '@angular/core/testing';
 import { OmdbService } from './omdb.service';
 
 describe('OmdbService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [OmdbService]
-    });
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [OmdbService]
+		});
+	});
 
-  it('should be created', inject([OmdbService], (service: OmdbService) => {
-    expect(service).toBeTruthy();
-  }));
+	it('should be created', inject([OmdbService], (service: OmdbService) => {
+		expect(service).toBeTruthy();
+	}));
 });
