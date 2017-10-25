9a681a6949380bf33e5f49808921afb1721cf2cd
David Geron-Neubauer <davidgn@udel.edu>
2017-10-25 19:11:35 -0400

    Basic GET request and data binding

--- Project2App/src/app/omdb/omdb.component.ts
+++ Project2App/src/app/omdb/omdb.component.ts
@@ -4,17 +4,18 @@ import { OmdbService } from './omdb.service';
 @Component({
 	selector: 'app-omdb',
 	templateUrl: './omdb.component.html',
-	styleUrls: ['./omdb.component.css'],
-	providers: [OmdbService]
+	styleUrls: ['./omdb.component.css']
 })
 export class OmdbComponent implements OnInit {
-	private nicInfo: JSON;
+	public nicInfo: JSON;
+	public temp: string;
 
 	constructor(private omdb: OmdbService) { }
 	// constructor() {}
 
 	ngOnInit() {
 		// this.nicInfo = OmdbComponent
+		this.temp = 'test temp';
 	}
 
 	/**
@@ -22,15 +23,17 @@ export class OmdbComponent implements OnInit {
 	 */
 	public letsGetSomeMovies() {
 		// console.log(evt);
-		alert(this.omdb.temp);
-		// this.omdb.getAllNicCageMovies().subscribe(
-		// 	(success) => {
-		// 		console.log(success);
-		// 		this.nicInfo = success.json();
-		// 	},
-		// 	(error) => {
-		// 		console.error(error);
-		// });
+		// alert(this.omdb.temp);
+		this.omdb.getAllNicCageMovies().subscribe(
+			(success) => {
+				console.log(success);
+				this.temp = 'wooo';
+				this.nicInfo = success.json();
+			},
+			(error) => {
+				this.temp = 'error';
+				console.error(error);
+		});
 	}
 
 }
