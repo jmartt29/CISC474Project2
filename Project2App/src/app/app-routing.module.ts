f58e24bc15b62b68e6cb546109b891247139ba37
hunterhhh <hedinger@udel.edu>
2017-10-30 14:59:43 -0400

    fixed routing for sure

--- Project2App/src/app/app-routing.module.ts
+++ Project2App/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//import { OmdbComponent } from './omdb/omdb.component';
+import { OmdbComponent } from './omdb/omdb.component';
 import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 	{ path: 'home', component: HomeComponent },
-	{ path: 'omdb', loadChildren: './omdb/omdb.module#OmdbModule' }
-	// { path: 'omdb', component: OmdbComponent }
+	//{ path: 'omdb', loadChildren: './omdb/omdb.module#OmdbModule' }
+	{ path: 'omdb', component: OmdbComponent }
 ];
 
 @NgModule({
